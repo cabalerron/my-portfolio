7163a6899e0a01bb9879767b59f84dc8b166d5bc
cabalerron <cabalerron@gmail.com>
2024-02-24 20:16:57 -0800

    Updated Home page

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I love you Bbyy lablab koo.<br/> 
-          Sorry na bat ang tagal ko mag reply.<br/>
-          Di ako marunong mambabae <br/>
-          may tinetest lang ako sa deployment ng App ko. Haha<br/>
-          saglit lang to. I miss youu!<br/>
+          ANU NAAAAA!!!!!! 
         </p>
         <a
           className="App-link"
