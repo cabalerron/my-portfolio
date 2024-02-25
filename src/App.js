8645cfe22fa3f07a28e5821afda0bb53c4513a55
cabalerron <cabalerron@gmail.com>
2024-02-24 19:55:18 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I love you Bbyy lablab koo.<br/> 
+          Sorry na bat ang tagal ko mag reply.<br/>
+          Di ako marunong mambabae <br/>
+          may tinetest lang ako sa deployment ng App ko. Haha<br/>
+          saglit lang to. I miss youu!<br/>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
